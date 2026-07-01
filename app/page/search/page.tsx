fb8a9992c74b1651b5c942bea5606aa6613fcfe6
Denis Semenyuk <semenyuk@MacBook-Air-Denis.local>
2026-07-01 17:55:24 +0500

    Страница с запросом, но пока чет не пашет, надо будет фиксить

--- app/page/search/page.tsx
+++ app/page/search/page.tsx
@@ -42,7 +42,36 @@ export default function SearchPage() {
       });
   }, [query]);
 
-  
+    return (
+        <main>
+            <h1>Результаты поиска: {query}</h1>
+            {loading && <div>Загрузка...</div>}
+            {error && <div style={{color:'red'}}>{error}</div>}
+            {!loading && !error && books.length === 0 && <p>Ничего не найдено.</p>}
+            <ul>
+                {books.map(book => {
+                    const v = book.volumeInfo;
+                    return (
+                        <li key={book.id} style={{marginBottom: 32}}>
+                            <strong>{v.title}</strong>
+                            {v.authors && <> - {v.authors.join(",")}</>}
+                            <div>
+                                {v.imageLinks?.thumbnail && (
+                                    <img src={v.imageLinks.thumbnail} alt={v.title} style={{height:110, marginTop: 6}} />
+                                )}
+                            </div>
+                            {v.description && <div style={{maxWidth: 430}}>{v.description.slice(0, 140)}...</div>}
+                            {v.infoLink && (
+                                <div>
+                                    <a href={v.infoLink} target='_blank' rel="noopener noreferrer">Подробнее</a>
+                                </div>
+                            )}
+                        </li>
+                    )
+                })}
+            </ul>
+        </main>
+    )
 
 
 
