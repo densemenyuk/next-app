2bfd9d3b770e853ef0bcc3cfa9cee9a4e39bf186
Denis Semenyuk <semenyuk@MacBook-Air-Denis.local>
2026-07-03 16:35:30 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/next-app',
+  assetPrefix:'/next-app',
   reactCompiler: true,
 };
 
